5bdb534e4f487062253bf11836762595c166fa54
Albert Portnoy <albert@albertp.dev>
2023-05-21 22:15:39 -0500

    Update example (credit @Catalyst4)

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,90 @@
-import { Injector, common, components, types, webpack } from "replugged";
-import { Channel, Guild } from "discord-types/general";
+import { Injector, components, types } from "replugged";
+import { GetContextItem } from "replugged/dist/types";
+
+const { MenuGroup, MenuItem } = components.ContextMenu;
 const { ContextMenuTypes } = types;
-const {
-  ContextMenu: { MenuItem },
-} = components;
-const {
-  constants: { Permissions },
-} = common;
-const { waitForProps } = webpack;
 
 const injector = new Injector();
 
-type ThreadMod = {
-  archiveThread: (threadId: string) => Promise<void>;
-  lockThread: (threadId: string) => Promise<void>;
-};
+export function start() {
+  const getItem: GetContextItem = (data, menu) => {
+    // return (
+    //   <MenuGroup>
+    //     <MenuItem
+    //       label="An item" // What shows to the user
+    //       id="id"
+    //       action={() => console.log(data, menu)} // What gets called when the item is clicked
+    //     />
+    //   </MenuGroup>
+    // );
+  };
 
-type CanMod = {
-  can: (permission: bigint, channel: Channel | Guild) => boolean;
-  getChannelPermissions: (channel: Channel | Guild) => bigint;
-};
+  // Registering the item
+  // First arg is the navId of the menu
+  // Second arg is the function  that gets called
+  injector.utils.addMenuItem(ContextMenuTypes.UserContext, getItem);
 
-type ThreadMenuType = {
-  channel: Channel;
-};
-
-export async function start() {
-  const { archiveThread, lockThread } = await waitForProps<keyof ThreadMod, ThreadMod>(
-    "archiveThread",
-    "lockThread",
+  // If you don't want to use jsx, you can instead return an object
+  // The attributes should correspond to the properties needed, except for `type`, which is the type of the item
+  injector.utils.addMenuItem(
+    ContextMenuTypes.Message,
+    () => {
+      const item = {
+        id: "item",
+        label: "Item",
+        action: () => console.log(item),
+        type: MenuItem,
+      };
+      return item;
+    },
+    2,
+    -1,
   );
-  const { can } = await waitForProps<keyof CanMod, CanMod>("can", "getChannelPermissions");
 
-  injector.utils.addMenuItem<ThreadMenuType>(ContextMenuTypes.ThreadContext, (data) => {
-    const { channel } = data;
-    const { archived, locked } = channel.threadMetadata;
-    const canManage = can(Permissions.MANAGE_THREADS, channel);
-    if (archived || locked || !canManage) {
-      return undefined;
-    }
-    return (
-      <MenuItem
-        id="close-and-lock"
-        label="Close and lock thread"
-        action={async () => {
-          await lockThread(channel.id);
-          await archiveThread(channel.id);
-        }}
-      />
-    );
-  });
+  // Registering an item also returns a function to remove the item if need be
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const callToRemove = injector.utils.addMenuItem(
+    ContextMenuTypes.ChannelContext,
+    (data, menu) => {
+      // You can add children to make the pop-out menu, like the "Invite to Server" on a user menu
+      return {
+        type: MenuItem,
+        label: "Hold on...",
+        // id: "abcd",
+        children: [
+          {
+            type: MenuItem,
+            label: "Woah a submenu!",
+            id: "1234",
+            action: () => console.log(menu),
+          },
+        ],
+      };
+
+      // Same as the above, but with jsx
+      // eslint-disable-next-line no-unreachable
+      <MenuItem label="Hold on..." id="abcd">
+        <MenuItem label="Woah a submenu!" id="1234" action={() => console.log(data)} />
+      </MenuItem>;
+    },
+    1,
+    1,
+  );
+
+  // Let's say you hate the copy-id button even existing, and want to remedy that
+  injector.utils.addMenuItem<{ children: unknown }>(
+    ContextMenuTypes.GuildContext,
+    (data, menu: { children: [] }) => {
+      // Simply edit the menu, and it'll no longer appear
+      console.log(menu);
+      menu.children.pop();
+
+      // We don't want to show an item, so we simply return nothing
+      // Anything falsy can be returned in place of an item, and discord ignores it
+    },
+  );
 }
 
 export function stop() {
-  injector.uninjectAll();
+  injector.uninjectAll(); // Would also remove any registered items
 }
